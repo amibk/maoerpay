250a2d29f2d5017f8c6a1da4f28032a1dd88ae5e
Tony May <26061368+amibk@users.noreply.github.com>
2019-11-09 17:26:21 +0800

    Add files via upload

---
+++ src/MaoerBase.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace MaoerPay;
+
+use Maoerpay\rsa2;
+/**
+ * 猫耳支付基类
+ * Class MaoerBase
+ */
+
+class MaoerBase
+{
+
+    /**
+     * CURL访问
+     * @param $url
+     * @param bool $params
+     * @param int $ispost
+     * @param array $header
+     * @param bool $verify
+     * @return bool|string
+     */
+    protected function http($url, $params = false, $ispost = 0, $header = [], $verify = false) {
+        $httpInfo = array();
+        $ch = curl_init();
+        if(!empty($header)){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        //忽略ssl证书
+        if($verify === true){
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        } else {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
+        if ($ispost) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            curl_setopt($ch, CURLOPT_URL, $url);
+        } else {
+            if (is_array($params)) {
+                $params = http_build_query($params);
+            }
+            if ($params) {
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+
+    /**
+     * 计算签名
+     * @param array $params
+     * @return string|null
+     */
+    protected function sign($params = []){
+        $str = $this->ToSignParams($params);
+        $sign = rsa2::createSign($str);
+        return $sign;
+    }
+
+    /**
+     * 参数合并获得待签名数据
+     * @param $value
+     * @return string
+     */
+    private function ToSignParams($value)
+    {
+        $buff = "";
+        foreach ($value as $k => $v) {
+            if ($k != "sign" && $v != "" && !is_array($v)) {
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+}
---
+++ src/Trade.php
@@ -0,0 +1,96 @@
+<?php
+
+
+namespace MaoerPay;
+
+use MaoerPay\MaoerBase;
+use MaoerPay\rsa2;
+/**
+ * 猫耳支付交易处理
+ * Class Trade
+ * @package MaoerPay
+ */
+class Trade extends MaoerBase
+{
+    //应用私钥
+    private $PRIVATE_KEY;
+    //商户号
+    private $mch_id;
+
+    public function __construct(array $params)
+    {
+        if (empty($params['mch_id'])){
+            return "缺少商户号";
+        }elseif (empty($params['PRIVATE_KEY'])){
+            return "应用私钥";
+        }
+        rsa2::$PRIVATE_KEY = $params['PRIVATE_KEY'];
+        $this->mch_id = $params['mch_id'];
+    }
+
+    public function barpay(array $params){
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/barpay',$params,1);
+        return $result;
+    }
+    public function jsapi(array $params){
+        if ($params['trade_type'] == "JSAPI" && empty($params['trade_type'])){
+            return 'JSAPI支付时，openid为必传参数';
+        }elseif ($params['NATIVE'] == "NATIVE" && empty($params['product_id'])){
+            return 'NATIVE支付时，product_id为必传参数';
+        }
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/jspay',$params,1);
+        return $result;
+    }
+    public function order_query(array $params){
+        if (empty($params['out_trade_no']) && empty($params['trade_type'])) return 'out_trade_no和maoer_id不能同时为空';
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/order_query',$params,1);
+        return $result;
+    }
+    public function order_close(array $params){
+        if (empty($params['out_trade_no']))return "请传递要关闭的订单号";
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/order_close',$params,1);
+        return $result;
+    }
+    public function order_reverse(array $params){
+        if (empty($params['out_trade_no']))return "请传递要撤销的订单号";
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/order_reverse',$params,1);
+        return $result;
+    }
+    public function refund(array $params){
+        $params['refund_desc'] = empty($params['refund_desc']) ? "正常退款" : $params['refund_desc'];
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/refund',$params,1);
+        return $result;
+    }
+    public function refund_query(array $params){
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/refund_query',$params,1);
+        return $result;
+    }
+    public function downloadbill(array $params){
+        $params['mchid'] = $this->mch_id;
+        $sign = $this->sign($params);
+        $params['sign'] = $sign;
+        $result = $this->http('https://open.maoer.net.cn/api/trade/downloadbill',$params,1);
+        return $result;
+    }
+}
